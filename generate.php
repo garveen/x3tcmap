0e4c5dccdb7ba567549bb18d33f8cf7b273d992c
Gavin <acabin@live.com>
2017-02-05 13:51:19 +0800

    ap universe

--- generate.php
+++ generate.php
@@ -10,14 +10,16 @@ $cellGapX = 10;
 $lineWidth = 10;
 
 foreach (['chinese', 'english'] as $language) {
-    $parser = new Parser("lang/{$language}.xml", ['maps/x3_universe.xml', 'maps/x3_universe_2.0.xml']);
-    ob_start();
-    include 'template.php';
-    $content = ob_get_clean();
-    file_put_contents("{$language}.html", $content);
+    foreach (['', 'ap'] as $map) {
+        $parser = new Parser("lang/{$language}.xml", ["maps/x3{$map}_universe.xml", "maps/x3_universe_2.0.xml"]);
+        ob_start();
+        include 'template.php';
+        $content = ob_get_clean();
+        file_put_contents("{$map}{$language}.html", $content);
+    }
 }
 
 if (file_exists('index.html')) {
-	unlink('index.html');
+    unlink('index.html');
 }
 rename('english.html', 'index.html');
